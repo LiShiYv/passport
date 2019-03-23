225493dd645ee7403161a83dadbed2bcdeb29d9f
LiShiYv <qq15354304250@163.com.com>
2019-03-23 10:10:01 +0800

    sswwqqq

--- app/Http/Controllers/User/UserController.php
+++ app/Http/Controllers/User/UserController.php
@@ -29,15 +29,15 @@ public function login(Request $request){
         $root=$request->input('u_name');
         $r=$request->input('redirect')?? env('SHOP_URL');
         //var_dump($r);die;
-        $id2 = Cmsmodel::where(['u_name'=>$root])->first();
+        $res = Cmsmodel::where(['u_name'=>$root])->first();
         //var_dump($id2);
-        if($id2){
-            if(password_verify($pass,$id2->pwd)){
+        if($res){
+            if(password_verify($pass,$res->pwd)){
                 $token = substr(md5(time().mt_rand(1,99999)),10,10);
                 setcookie('token',$token,time()+86400,'/','sub.52xiuge.com',false,true);
-                setcookie('u_name',$id2->u_name,time()+86400,'/','sub.52xiuge.com',false,true);
-                setcookie('id',$id2->id,time()+86400,'/','sub.52xiuge.com',false,true);
-                $redis_key_web_token='str:u:token:'.$id2->uid;
+                setcookie('u_name',$res->u_name,time()+86400,'/','sub.52xiuge.com',false,true);
+                setcookie('id',$res->id,time()+86400,'/','sub.52xiuge.com',false,true);
+                $redis_key_web_token='str:u:token:'.$res->uid;
                 Redis::del($redis_key_web_token);
                 Redis::hSet($redis_key_web_token,'web',$token);
 
